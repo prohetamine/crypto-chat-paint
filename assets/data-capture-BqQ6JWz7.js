f81accf424fa61dceb7e764309989b8ca49893fe
Stas Prohetamine <prohetamine@gmail.com>
2026-01-13 01:54:13 +0300

    deploy

--- assets/data-capture-BqQ6JWz7.js
+++ assets/data-capture-BqQ6JWz7.js
@@ -1,4 +1,4 @@
-import{g as y,i as $,b as l,n as S,a as x,O as m,R as p,k as g,S as h,D as u,U as D,H as E,I as R}from"./index-so_qv9ph.js";import{W as C}from"./index-DbjnCFjJ.js";import{ReownAuthentication as v}from"./features-D7iyfTJG.js";const _=y`
+import{g as y,i as $,b as l,n as S,a as x,O as m,R as p,k as g,S as h,D as u,U as D,H as E,I as R}from"./index-BjreG8oW.js";import{W as C}from"./index-KCsqQdmP.js";import{ReownAuthentication as v}from"./features-P2wSN_UU.js";const _=y`
   .email-sufixes {
     display: flex;
     flex-direction: row;
--- assets/transactions-Bk2Aw6E1.js
+++ assets/transactions-Bk2Aw6E1.js
@@ -1,4 +1,4 @@
-import{g as f,i as a,b as d,a as u}from"./index-so_qv9ph.js";const w=f`
+import{g as f,i as a,b as d,a as u}from"./index-BjreG8oW.js";const w=f`
   :host > wui-flex:first-child {
     height: 500px;
     overflow-y: auto;

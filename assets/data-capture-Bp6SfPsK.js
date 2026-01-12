62b06b51eaea5e2de3b2ef83fa0defb5f5db0f46
Stas Prohetamine <prohetamine@gmail.com>
2026-01-13 02:28:33 +0300

    deploy

--- assets/data-capture-Bp6SfPsK.js
+++ assets/data-capture-Bp6SfPsK.js
@@ -1,4 +1,4 @@
-import{g as y,i as $,b as l,n as S,a as x,O as m,R as p,k as g,S as h,D as u,U as D,H as E,I as R}from"./index-DoU4KGCE.js";import{W as C}from"./index-D5cjtbr8.js";import{ReownAuthentication as v}from"./features-oasUgL28.js";const _=y`
+import{g as y,i as $,b as l,n as S,a as x,O as m,R as p,k as g,S as h,D as u,U as D,H as E,I as R}from"./index-CKlmNYFp.js";import{W as C}from"./index-7MGI5KTO.js";import{ReownAuthentication as v}from"./features-Ce7C5oZR.js";const _=y`
   .email-sufixes {
     display: flex;
     flex-direction: row;
